833ab5ddee135fe18aa77f76af70009b84ba70e9
liuzenan2 <695810461@qq.com>
2020-03-29 23:15:35 +0800

    '第三次提交'

---
+++ controls/userControl.js
@@ -0,0 +1,47 @@
+const UserModel = require("../db/model/userModel")
+//添加用户
+let addUser = async ({mail,pass}) => {
+  let result = await UserModel.insertMany({mail,pass})
+  return result
+}
+//删除用户
+let delUser = async (_id) => {
+  let result = await UserModel.deleteOne({ _id })
+  return result
+}
+
+// 注册
+let userReg = async (mail, pass) => {
+  //  1. 邮箱是否重复
+  let isExst = await UserModel.findOne({ mail })
+  let result
+  // 如果查询到数据 返回查到的数据 没有返回假 
+  if (isExst) {
+    throw '邮箱已注册'
+  } else {
+    result = await UserModel.insertMany({ mail, pass })
+  }
+  //  2. 插入数据库
+  return result
+}
+
+// 用户登录
+let userLogin = async (mail, pass) => {
+  let result = await UserModel.findOne({ mail, pass })
+  return result
+}
+
+// 查询全部用户
+let findAllUser = async () => {
+  let result = await UserModel.find()
+  return result
+}
+
+
+module.exports = {
+  userReg,
+  userLogin,
+  findAllUser,
+  delUser,
+  addUser
+}
--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express=require('express')
 const bodyParser=require('body-parser');
+const path = require('path')
+// const tokenMiddleWare = require('./middleware/tokenMiddleWare')
 const app =express()
 const db=require('./db/connect')
+
 
 //post 数据的解析 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+//静态资源路径
+
 app.use('/public',express.static(path.join(__dirname,'./public')))
 
 let goodsRouter=require('./router/goodsRouter')
-app.use('/admin/goods',goodsRouter)
+let userRouter = require('./router/userRouter')
 
-let kindRouter=require('./router/foodRouter')
+
+let kindRouter=require('./router/kindRouter')
 let uploadRouer=require('./router/uploadRouter')
+
+
+
+app.use('/admin/goods',goodsRouter)
+app.use('/admin/user',userRouter)
+app.use('/admin/user',kindRouter)
+app.use('/admin/upload',uploadRouer)
 
 app.listen(3000,()=>{
     console.log(`/**
